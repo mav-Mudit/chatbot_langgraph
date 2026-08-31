a91b56aa21bb4fad58cad21e897873f718ca7ea3
Mudit <mudittandon34@gmail.com>
2026-08-31 17:00:16 +0200

    feat: add persistent storage

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from langgraph_backend import chatbot
+from langgraph_backend import chatbot, retrieve_all_threads
 from langchain_core.messages import HumanMessage, AIMessage
 import uuid
+
 
 # utility functions for managing chat threads and session state
 
@@ -33,7 +34,7 @@ if 'thread_id' not in st.session_state:
     st.session_state['thread_id'] = generate_thread_id()
 
 if 'chat_threads' not in st.session_state:
-    st.session_state['chat_threads'] = []
+    st.session_state['chat_threads'] = retrieve_all_threads()
 
 add_thread(st.session_state['thread_id'])
 
